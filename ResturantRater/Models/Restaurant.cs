ef0f903a4c1965efe0efc4fbd3560977b27ee706
dawita <dawit.fitru@gmail.com>
2021-03-23 19:04:24 -0400

    Data Layer push

---
+++ ResturantRater/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResturantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestauramtId { get; set; }
+        public int Name { get; set; }
+        public int Adress { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
